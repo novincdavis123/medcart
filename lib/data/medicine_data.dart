9b52ae65010e51cbe126e3b6fd052e52c486a035
novincdavis123 <novindavisks@gmail.com>
2025-12-17 20:08:32 +0530

    medicine data entries
    ADDED
     - Created medicine_data.dart
     - 4 medicine items
    EDITED
     - renamed product to medicinemodel class

---
+++ lib/data/medicine_data.dart
@@ -0,0 +1,32 @@
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:medcart/models/medicine.dart';
+
+final medicinesProvider = Provider<List<MedicineModel>>((ref) {
+  return [
+    MedicineModel(
+      id: 1,
+      name: 'Paracetamol 500mg',
+      price: 50,
+      image: 'assets/images/placeholder.png',
+    ),
+    MedicineModel(
+      id: 2,
+      name: 'Vitamin C Tablets',
+      price: 120,
+      image: 'assets/images/placeholder.png',
+    ),
+    MedicineModel(
+      id: 3,
+      name: 'Blood Pressure Monitor',
+      price: 1800,
+      image: 'assets/images/placeholder.png',
+    ),
+    MedicineModel(
+      id: 4,
+      name: 'Thermometer',
+      price: 300,
+      image: 'assets/images/placeholder.png',
+    ),
+  ];
+});
--- lib/models/medicine.dart
+++ lib/models/medicine.dart
@@ -1,10 +1,10 @@
-class Product {
+class MedicineModel {
   final int id;
   final String name;
   final double price;
   final String image; // placeholder image
 
-  Product({
+  MedicineModel({
     required this.id,
     required this.name,
     required this.price,
@@ -14,7 +14,7 @@ class Product {
   Map<String, dynamic> toJson() =>
       {'id': id, 'name': name, 'price': price, 'image': image};
 
-  factory Product.fromJson(Map<String, dynamic> json) => Product(
+  factory MedicineModel.fromJson(Map<String, dynamic> json) => MedicineModel(
         id: json['id'],
         name: json['name'],
         price: json['price'],
